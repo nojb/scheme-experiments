39ad188b4bbecb49b71ddbb394709840b71ee5e8
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2016-11-11 14:45:56 +0100

    Add immediate types: char, bool, fixnum

--- c.ss
+++ c.ss
@@ -9,8 +9,22 @@
 (define (emit-symbol s)
   (printf "_~a:\n" s))
 
+(define fixnum-shift 1)
+(define fixnum-tag 1)
+
+(define boolean-shift 4)
+(define boolean-tag #b0110)
+
+(define char-shift 8)
+(define char-tag #b1010)
+
 (define (compile-program x)
-  (emit "movl	$~a, %eax" x)
+  (define (immediate-rep x)
+    (cond
+     ((integer? x) (fxlogor (fxsll x fixnum-shift) fixnum-tag))
+     ((boolean? x) (fxlogor (fxsll (if x 1 0) boolean-shift) boolean-tag))
+     ((char? x) (fxlogor (fxsll (char->integer x) char-shift) char-tag))))
+  (emit "movl	$~a, %eax" (immediate-rep x))
   (emit "retq"))
 
 (define (compile-all x)
--- driver.c
+++ driver.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
+#define fixnum_mask      1
+#define fixnum_shift     1
+#define fixnum_tag       1
+
+#define boolean_mask     0xf
+#define boolean_shift    4
+#define boolean_tag      0x6
+
+#define char_mask        0xff
+#define char_shift       8
+#define char_tag         10
+
 extern int scheme_entry (void);
 
 int main (int argc, char **argv)
 {
-  printf ("%d\n", scheme_entry ());
+  int val = scheme_entry ();
+  if ((val & fixnum_mask) == fixnum_tag) {
+    printf ("%d\n", val >> fixnum_shift);
+  } else if ((val & boolean_mask) == boolean_tag) {
+    printf ("%s\n", (val >> boolean_shift) ? "#t" : "#f");
+  } else if ((val & char_mask) == char_tag) {
+    printf ("#\\%c\n", (char) (val >> char_shift));
+  } else {
+    printf ("uknown type\n");
+  }
   return 0;
 }
